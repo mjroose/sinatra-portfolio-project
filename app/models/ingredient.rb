d5570d195dc5ac2a69b5a4a0aec4994bab18b6eb
mjroose <mjroose@gmail.com>
2018-11-08 14:05:54 -0500

    Style recipes/show view

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,22 @@ class Ingredient < ActiveRecord::Base
   belongs_to :food
   belongs_to :recipe
 
+  def name_and_amount
+    message = "#{self.food.name} "
+
+    if self.quantity != "" && self.unit != ""
+      message += "(#{self.quantity} #{self.unit})"
+    elsif self.quantity != ""
+      message += "(#{self.quantity})"
+    elsif self.unit != ""
+      message += "(#{self.unit})"
+    else
+      message += ""
+    end
+  
+    message
+  end
+
   def self.find_or_create_from_rows(recipe, rows = [])
     self.clean_ingredient_rows(rows).each do |ingredient|
       Ingredient.find_or_create_by(
